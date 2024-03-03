ac9027eb699af9f4c9045d6cd5abb5a77a281269
ifshaulhaque <ifshaul.haque@gmail.com>
2024-03-03 14:42:04 +0530

    create user

--- api/auth/controller.js
+++ api/auth/controller.js
@@ -1,5 +1,35 @@
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { firebase } from "../../db/firebase.js";
+import User from "./userModel.js";
+
+const auth = getAuth(firebase.getFirebaseApp());
+
 export const controller = {
-    createUserWithEmailAndPassword : (req, res) => {
-        res.json({requestBody: req.body});
+    createUserWithEmailAndPassword: (req, res) => {
+        let email = req.body.email;
+        let password = req.body.password;
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                var user = new User({
+                    _id: userCredential.user.uid,
+                    email: userCredential.user.email,
+                    emailVerified: userCredential.user.emailVerified,
+                    isAnonymous: userCredential.user.isAnonymous,
+                    providerData: userCredential.user.providerData,
+                    stsTokenManager: userCredential.user.stsTokenManager,
+                    metadata: userCredential.user.metadata,
+                })
+
+                user.save()
+                    .then((result) => {
+                        res.json(result);
+                    })
+                    .catch((error) => {
+                        res.json(error);
+                    })
+            })
+            .catch((error) => {
+                res.json(error);
+            });
     }
 }
