66b93d35862cb76827014e848a61325efb43b93a
ifshaulhaque <ifshaul.haque@gmail.com>
2024-03-16 16:26:21 +0530

    adding cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from 'cors';
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./swagger.js";
 import authRoute from "./api/auth/route.js";
@@ -11,6 +12,8 @@ import "./db/firebase.js"
 
 dotenv.config();
 const app = express();
+
+app.use(cors());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
